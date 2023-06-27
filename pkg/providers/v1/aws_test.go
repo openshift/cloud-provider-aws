3c839f251dd2d6efb809740afef192c8935469d8
Joel Speed <joel.speed@hotmail.co.uk>
2023-06-27 17:50:51 +0100

    UPSTREAM: 622: Update health checks to use Kube-Proxy when no other configuration is provided

--- pkg/providers/v1/aws_test.go
+++ pkg/providers/v1/aws_test.go
@@ -3278,7 +3278,7 @@ func TestNLBNodeRegistration(t *testing.T) {
 		}
 	}
 
-	fauxService.Annotations[ServiceAnnotationLoadBalancerHealthCheckProtocol] = "http"
+	fauxService.Annotations[ServiceAnnotationLoadBalancerHealthCheckProtocol] = "tcp"
 	tgARN := aws.StringValue(awsServices.elbv2.(*MockedFakeELBV2).Listeners[0].DefaultActions[0].TargetGroupArn)
 	_, err = c.EnsureLoadBalancer(context.TODO(), TestClusterName, fauxService, nodes)
 	if err != nil {
@@ -3431,8 +3431,9 @@ func TestCloud_buildNLBHealthCheckConfiguration(t *testing.T) {
 				},
 			},
 			want: healthCheckConfig{
-				Port:               "traffic-port",
-				Protocol:           elbv2.ProtocolEnumTcp,
+				Port:               "10256",
+				Protocol:           elbv2.ProtocolEnumHttp,
+				Path:               "/healthz",
 				Interval:           30,
 				Timeout:            10,
 				HealthyThreshold:   3,
@@ -3610,8 +3611,9 @@ func TestCloud_buildNLBHealthCheckConfiguration(t *testing.T) {
 				},
 			},
 			want: healthCheckConfig{
-				Port:               "traffic-port",
-				Protocol:           elbv2.ProtocolEnumTcp,
+				Port:               "10256",
+				Protocol:           elbv2.ProtocolEnumHttp,
+				Path:               "/healthz",
 				Interval:           23,
 				Timeout:            10,
 				HealthyThreshold:   3,
@@ -3668,8 +3670,9 @@ func TestCloud_buildNLBHealthCheckConfiguration(t *testing.T) {
 				},
 			},
 			want: healthCheckConfig{
-				Port:               "traffic-port",
-				Protocol:           elbv2.ProtocolEnumTcp,
+				Port:               "10256",
+				Protocol:           elbv2.ProtocolEnumHttp,
+				Path:               "/healthz",
 				Interval:           30,
 				Timeout:            10,
 				HealthyThreshold:   7,
