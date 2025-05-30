78cb592d5d5c7ccbad0305dd2447b6ffcab80f5f
Marco Braga <mrbraga@redhat.com>
2025-05-29 23:35:41 -0300

    e2e: enhance e2e to support NLB tests on OCP

--- tests/e2e/loadbalancer.go
+++ tests/e2e/loadbalancer.go
@@ -17,7 +17,6 @@ import (
 	"context"
 	"fmt"
 	"strings"
-	"time"
 
 	"github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/ginkgo/v2"
@@ -35,133 +34,7 @@ import (
 	elbv2 "github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2"
 )
 
-type configServiceLB struct {
-	PodPort            uint16
-	PodProtocol        v1.Protocol
-	LoadBalancerType   string
-	DefaultAnnotations map[string]string
-
-	LBJig *e2eservice.TestJig
-}
-
-func newConfigServiceLB() *configServiceLB {
-	return &configServiceLB{
-		PodPort:     8080,
-		PodProtocol: v1.ProtocolTCP,
-		DefaultAnnotations: map[string]string{
-			"aws-load-balancer-backend-protocol": "http",
-			"aws-load-balancer-ssl-ports":        "https",
-		},
-		LoadBalancerType: "classic",
-	}
-}
-
-func (s *configServiceLB) newServiceTemplate() *v1.Service {
-	return &v1.Service{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: s.LBJig.Namespace,
-			Name:      s.LBJig.Name,
-			Labels:    s.LBJig.Labels,
-		},
-		Spec: v1.ServiceSpec{
-			Selector: s.LBJig.Labels,
-			Ports: []v1.ServicePort{
-				{
-					Protocol: s.PodProtocol,
-					Port:     int32(s.PodPort),
-				},
-			},
-		},
-	}
-}
-
-func (s *configServiceLB) buildService(extraAnnotations map[string]string) *v1.Service {
-	svc := s.newServiceTemplate()
-	svc.Annotations = make(map[string]string, len(s.DefaultAnnotations)+len(extraAnnotations))
-	// add default annotations - can be overriden by extra annotations
-	for aK, aV := range s.DefaultAnnotations {
-		svc.Annotations[aK] = aV
-	}
-
-	// append test case annotations to the service
-	for aK, aV := range extraAnnotations {
-		svc.Annotations[aK] = aV
-	}
-
-	svc.Spec.Type = v1.ServiceTypeLoadBalancer
-	// We need to turn affinity off for our LB distribution tests
-	svc.Spec.SessionAffinity = v1.ServiceAffinityNone
-	svc.Spec.Ports = []v1.ServicePort{
-		{
-			Name:       "http",
-			Protocol:   v1.ProtocolTCP,
-			Port:       int32(80),
-			TargetPort: intstr.FromInt(int(s.PodPort)),
-		},
-		{
-			Name:       "https",
-			Protocol:   v1.ProtocolTCP,
-			Port:       int32(443),
-			TargetPort: intstr.FromInt(int(s.PodPort)),
-		},
-	}
-	return svc
-}
-
-func (s *configServiceLB) buildReplicaSet() func(rc *v1.ReplicationController) {
-	return func(rc *v1.ReplicationController) {
-		var replicas int32 = 1
-		var grace int64 = 3 // so we don't race with kube-proxy when scaling up/down
-		rc.ObjectMeta = metav1.ObjectMeta{
-			Namespace: s.LBJig.Namespace,
-			Name:      s.LBJig.Name,
-			Labels:    s.LBJig.Labels,
-		}
-		rc.Spec = v1.ReplicationControllerSpec{
-			Replicas: &replicas,
-			Selector: s.LBJig.Labels,
-			Template: &v1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: s.LBJig.Labels,
-				},
-				Spec: v1.PodSpec{
-					Containers: []v1.Container{
-						{
-							Name:  "netexec",
-							Image: imageutils.GetE2EImage(imageutils.Agnhost),
-							Args: []string{
-								"netexec",
-								fmt.Sprintf("--http-port=%d", s.PodPort),
-								fmt.Sprintf("--udp-port=%d", s.PodPort),
-							},
-							ReadinessProbe: &v1.Probe{
-								PeriodSeconds: 3,
-								ProbeHandler: v1.ProbeHandler{
-									HTTPGet: &v1.HTTPGetAction{
-										Port: intstr.FromInt(int(s.PodPort)),
-										Path: "/hostName",
-									},
-								},
-							},
-						},
-					},
-					TerminationGracePeriodSeconds: &grace,
-				},
-			},
-		}
-	}
-}
-
-func (s *configServiceLB) createLoadBalancerService(svc *v1.Service, timeout time.Duration) (*v1.Service, error) {
-	_, err := s.LBJig.Client.CoreV1().Services(s.LBJig.Namespace).Create(context.TODO(), svc, metav1.CreateOptions{})
-	if err != nil {
-		return nil, fmt.Errorf("failed to create LoadBalancer Service %q: %v", svc.Name, err)
-	}
-
-	ginkgo.By("waiting for loadbalancer for service " + s.LBJig.Namespace + "/" + s.LBJig.Name)
-	return s.LBJig.WaitForLoadBalancer(timeout)
-}
-
+// loadbalancer tests
 var _ = Describe("[cloud-provider-aws-e2e] loadbalancer", func() {
 	f := framework.NewDefaultFramework("cloud-provider-aws")
 	f.NamespacePodSecurityEnforceLevel = admissionapi.LevelPrivileged
@@ -217,12 +90,7 @@ var _ = Describe("[cloud-provider-aws-e2e] loadbalancer", func() {
 				workerNodes := len(nodeList.Items)
 				framework.Logf("Found %d worker nodes", workerNodes)
 
-				// Validate in the TG if the node count matches with expected label selector.
-				// 1. Get Load Balancer ARN from DNS name in svc.Status.LoadBalancer.Ingress[0].Hostname
-				// 2. List listeners for the load balancer
-				// 3. Get target groups attached to listeners
-				// 4. Count registered targets in target groups
-				// 5. Verify count matches number of worker nodes
+				// Validate in the TG if the node count matches with expected target-node-labels selector.
 				lbDNS := svc.Status.LoadBalancer.Ingress[0].Hostname
 				framework.ExpectNoError(getLBTargetCount(context.TODO(), lbDNS, workerNodes), "AWS LB target count validation failed")
 			},
@@ -235,23 +103,30 @@ var _ = Describe("[cloud-provider-aws-e2e] loadbalancer", func() {
 			loadBalancerCreateTimeout := e2eservice.GetServiceLoadBalancerCreationTimeout(cs)
 			framework.Logf("Running tests against AWS with timeout %s", loadBalancerCreateTimeout)
 
+			// Create Configuration
 			serviceName := serviceNameBase
 			if len(tc.ResourceSuffix) > 0 {
 				serviceName = fmt.Sprintf("%s-%s", serviceName, tc.ResourceSuffix)
 			}
 			framework.Logf("namespace for load balancer conig test: %s", ns.Name)
 
-			lbConfig := newConfigServiceLB()
-
 			By("creating a TCP service " + serviceName + " with type=LoadBalancerType in namespace " + ns.Name)
+			lbConfig := newConfigServiceLB()
 			lbConfig.LBJig = e2eservice.NewTestJig(cs, ns.Name, serviceName)
-
 			lbServiceConfig := lbConfig.buildService(tc.Annotations)
-			lbService, err := lbConfig.createLoadBalancerService(lbServiceConfig, loadBalancerCreateTimeout)
+
+			// Create Load Balancer
+			ginkgo.By("creating loadbalancer for service " + lbServiceConfig.Namespace + "/" + lbServiceConfig.Name)
+			_, err := lbConfig.LBJig.Client.CoreV1().Services(lbConfig.LBJig.Namespace).Create(context.TODO(), lbServiceConfig, metav1.CreateOptions{})
+			framework.ExpectNoError(fmt.Errorf("failed to create LoadBalancer Service %q: %v", lbServiceConfig.Name, err))
+
+			ginkgo.By("waiting for loadbalancer for service " + lbServiceConfig.Namespace + "/" + lbServiceConfig.Name)
+			lbService, err := lbConfig.LBJig.WaitForLoadBalancer(loadBalancerCreateTimeout)
 			framework.ExpectNoError(err)
 
+			// Run Workloads
 			By("creating a pod to be part of the TCP service " + serviceName)
-			_, err = lbConfig.LBJig.Run(lbConfig.buildReplicaSet())
+			_, err = lbConfig.LBJig.Run(lbConfig.buildReplicationController())
 			framework.ExpectNoError(err)
 
 			if tc.PostRunValidations != nil {
@@ -259,6 +134,7 @@ var _ = Describe("[cloud-provider-aws-e2e] loadbalancer", func() {
 				tc.PostRunValidations(lbConfig, lbService)
 			}
 
+			// Test the Service Endpoint
 			By("hitting the TCP service's LB External IP")
 			svcPort := int(lbService.Spec.Ports[0].Port)
 			ingressIP := e2eservice.GetIngressPoint(&lbService.Status.LoadBalancer.Ingress[0])
@@ -280,8 +156,126 @@ var _ = Describe("[cloud-provider-aws-e2e] loadbalancer", func() {
 	}
 })
 
-// Extra AWS lookup
-// getLBTargetCount verifies the number of registered targets for a given LB DNS name matches the expected count.
+// configServiceLB hold loadbalancer test configurations
+type configServiceLB struct {
+	PodPort            uint16
+	PodProtocol        v1.Protocol
+	DefaultAnnotations map[string]string
+
+	LBJig *e2eservice.TestJig
+}
+
+func newConfigServiceLB() *configServiceLB {
+	return &configServiceLB{
+		PodPort:     8080,
+		PodProtocol: v1.ProtocolTCP,
+		DefaultAnnotations: map[string]string{
+			"aws-load-balancer-backend-protocol": "http",
+			"aws-load-balancer-ssl-ports":        "https",
+		},
+	}
+}
+
+func (s *configServiceLB) buildService(extraAnnotations map[string]string) *v1.Service {
+	svc := &v1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace:   s.LBJig.Namespace,
+			Name:        s.LBJig.Name,
+			Labels:      s.LBJig.Labels,
+			Annotations: make(map[string]string, len(s.DefaultAnnotations)+len(extraAnnotations)),
+		},
+		Spec: v1.ServiceSpec{
+			Type:            v1.ServiceTypeLoadBalancer,
+			SessionAffinity: v1.ServiceAffinityNone,
+			Selector:        s.LBJig.Labels,
+			Ports: []v1.ServicePort{
+				{
+					Name:       "http",
+					Protocol:   v1.ProtocolTCP,
+					Port:       int32(80),
+					TargetPort: intstr.FromInt(int(s.PodPort)),
+				},
+				{
+					Name:       "https",
+					Protocol:   v1.ProtocolTCP,
+					Port:       int32(443),
+					TargetPort: intstr.FromInt(int(s.PodPort)),
+				},
+			},
+		},
+	}
+
+	// add default annotations - can be overriden by extra annotations
+	for aK, aV := range s.DefaultAnnotations {
+		svc.Annotations[aK] = aV
+	}
+
+	// append test case annotations to the service
+	for aK, aV := range extraAnnotations {
+		svc.Annotations[aK] = aV
+	}
+
+	return svc
+}
+
+// buildReplicationController creates a replication controller wrapper for the test framework.
+// buildReplicationController is basaed on newRCTemplate() from the test, which not provide
+// customization to bind in non-privileged ports.
+// TODO(mtulio): v1.33+[2] moved from RC to Deployments on tests, we must do the same to use Run()
+// when the test framework is updated.
+// [1] https://github.com/kubernetes/kubernetes/blob/89d95c9713a8fd189e8ad555120838b3c4f888d1/test/e2e/framework/service/jig.go#L636
+// [2] https://github.com/kubernetes/kubernetes/issues/119021
+func (s *configServiceLB) buildReplicationController() func(rc *v1.ReplicationController) {
+	return func(rc *v1.ReplicationController) {
+		var replicas int32 = 1
+		var grace int64 = 3 // so we don't race with kube-proxy when scaling up/down
+		rc.ObjectMeta = metav1.ObjectMeta{
+			Namespace: s.LBJig.Namespace,
+			Name:      s.LBJig.Name,
+			Labels:    s.LBJig.Labels,
+		}
+		rc.Spec = v1.ReplicationControllerSpec{
+			Replicas: &replicas,
+			Selector: s.LBJig.Labels,
+			Template: &v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: s.LBJig.Labels,
+				},
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name:  "netexec",
+							Image: imageutils.GetE2EImage(imageutils.Agnhost),
+							Args: []string{
+								"netexec",
+								fmt.Sprintf("--http-port=%d", s.PodPort),
+								fmt.Sprintf("--udp-port=%d", s.PodPort),
+							},
+							ReadinessProbe: &v1.Probe{
+								PeriodSeconds: 3,
+								ProbeHandler: v1.ProbeHandler{
+									HTTPGet: &v1.HTTPGetAction{
+										Port: intstr.FromInt(int(s.PodPort)),
+										Path: "/hostName",
+									},
+								},
+							},
+						},
+					},
+					TerminationGracePeriodSeconds: &grace,
+				},
+			},
+		}
+	}
+}
+
+// getLBTargetCount verifies the number of registered targets for a given LBv2 DNS name matches the expected count.
+// The steps includes:
+// 1. Get Load Balancer ARN from DNS name extracted from service Status.LoadBalancer.Ingress[0].Hostname
+// 2. List listeners for the load balancer
+// 3. Get target groups attached to listeners
+// 4. Count registered targets in target groups
+// 5. Verify count matches number of worker nodes
 func getLBTargetCount(ctx context.Context, lbDNSName string, expectedTargets int) error {
 	// Load AWS config
 	cfg, err := config.LoadDefaultConfig(ctx)
@@ -290,7 +284,7 @@ func getLBTargetCount(ctx context.Context, lbDNSName string, expectedTargets int
 	}
 	elbClient := elbv2.NewFromConfig(cfg)
 
-	// 1. Get Load Balancer ARN from DNS name
+	// Get Load Balancer ARN from DNS name
 	describeLBs, err := elbClient.DescribeLoadBalancers(ctx, &elbv2.DescribeLoadBalancersInput{})
 	if err != nil {
 		return fmt.Errorf("failed to describe load balancers: %v", err)
@@ -306,7 +300,7 @@ func getLBTargetCount(ctx context.Context, lbDNSName string, expectedTargets int
 		return fmt.Errorf("could not find LB with DNS name: %s", lbDNSName)
 	}
 
-	// 2. List listeners for the load balancer
+	// List listeners for the load balancer
 	listenersOut, err := elbClient.DescribeListeners(ctx, &elbv2.DescribeListenersInput{
 		LoadBalancerArn: aws.String(lbARN),
 	})
@@ -314,7 +308,7 @@ func getLBTargetCount(ctx context.Context, lbDNSName string, expectedTargets int
 		return fmt.Errorf("failed to describe listeners: %v", err)
 	}
 
-	// 3. Get target groups attached to listeners
+	// Get target groups attached to listeners
 	targetGroupARNs := map[string]struct{}{}
 	for _, listener := range listenersOut.Listeners {
 		if len(targetGroupARNs) > 0 {
@@ -328,7 +322,7 @@ func getLBTargetCount(ctx context.Context, lbDNSName string, expectedTargets int
 		}
 	}
 
-	// 4. Count registered targets in target groups
+	// Count registered targets in target groups
 	totalTargets := 0
 	for tgARN := range targetGroupARNs {
 		tgHealth, err := elbClient.DescribeTargetHealth(ctx, &elbv2.DescribeTargetHealthInput{
@@ -340,7 +334,7 @@ func getLBTargetCount(ctx context.Context, lbDNSName string, expectedTargets int
 		totalTargets += len(tgHealth.TargetHealthDescriptions)
 	}
 
-	// 5. Verify count matches number of worker nodes
+	// Verify count matches number of worker nodes
 	if totalTargets != expectedTargets {
 		return fmt.Errorf("target count mismatch: expected %d, got %d", expectedTargets, totalTargets)
 	}
